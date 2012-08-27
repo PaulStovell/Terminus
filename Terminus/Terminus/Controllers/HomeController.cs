b2d7a799c0abab4cd925d13f53128c52445c7e88
Andrew Stovell <andrewhstovell@gmail.com>
2012-08-27 18:37:18 +0930

    Created Mvc Website Project
    Created Mvc Website Project
    Changed the CSS to redallover, a free CSS from www.freecsstemplates.org
    Added About page with details about Terminus

---
+++ Terminus/Terminus/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Terminus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Terminus!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            ViewBag.Message = "What is Terminus?";
+
+            return View();
+        }
+    }
+}
